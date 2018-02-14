33781d9710ba382ba7fc65c4ba933ff98d64c15a
Jon-Erik <jdc151@case.edu>
2018-02-14 11:31:10 -0500

    initial files

---
+++ assets/javascript/game.js
@@ -0,0 +1,9 @@
+var goalRandomNumber = Math.floor(Math.random() * (120 - 19) + 19);
+console.log(goalRandomNumber);
+
+var crystal1 = Math.floor(Math.random() * (12 - 1) + 1);
+var crystal2 = Math.floor(Math.random() * (12 - 1) + 1);
+var crystal3 = Math.floor(Math.random() * (12 - 1) + 1);
+var crystal4 = Math.floor(Math.random() * (12 - 1) + 1);
+
+console.log(crystal1 + " " + crystal2 + " " + crystal3 + " " + crystal4);
