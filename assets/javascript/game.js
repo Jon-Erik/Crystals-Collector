740c28a083669a945ec40fcd746a1ee5c132cb23
Jon-Erik <jdc151@case.edu>
2018-03-30 11:19:27 -0400

    added comments to code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,12 +1,15 @@
+//Function to generate a random number between and including two numbers
 var numberGenerator = function(min, max) {
 	min = Math.ceil(min);
 	max = Math.floor(max);
 	return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+//Function to generate a random number the player will try to add up to
 var goalRandomNumber = numberGenerator(19, 120);
 	console.log(goalRandomNumber);
 
+//Values of crystals are set randomly
 var crystal1 = numberGenerator(1, 12);
 var crystal2 = numberGenerator(1, 12);
 var crystal3 = numberGenerator(1, 12);
@@ -24,9 +27,10 @@ $("#wins").html(wins);
 $("#losses").html(losses);
 $("#reset").click(function(event) {
 	resetGame();
-	console.log(crystal1 + " " + crystal2 + " " + crystal3 + " " + crystal4);
+	//console.log(crystal1 + " " + crystal2 + " " + crystal3 + " " + crystal4);
 });
 
+//Resets the game and gives new values to each crystal
 var resetGame = function() {
 	goalRandomNumber = numberGenerator(19, 120);
 	crystal1 = numberGenerator(1, 12);
@@ -42,23 +46,25 @@ var resetGame = function() {
 	$('#messagebox').html("Click on a crystal to start playing!")
 }
 
+//Checks if the score added up by the player equals or is greater than the goal number
 var winChecker = function() {
 	if (totalScore === goalRandomNumber) {
 		$("#messagebox").html("You Win! Click reset to play again");
 		wins++;
 		$("#wins").html(wins);
 		gameStarted = false;
-		console.log(crystal1 + " " + crystal2 + " " + crystal3 + " " + crystal4);
+		//console.log(crystal1 + " " + crystal2 + " " + crystal3 + " " + crystal4);
 		
 	} else if (totalScore > goalRandomNumber) {
 		$("#messagebox").html("You Lose! Click reset to play again");
 		losses++;
 		$("#losses").html(losses);
 		gameStarted = false;
-		console.log(crystal1 + " " + crystal2 + " " + crystal3 + " " + crystal4);
+		//console.log(crystal1 + " " + crystal2 + " " + crystal3 + " " + crystal4);
 	}
 }
 
+//Adds value of crystal clicked to total score and checks if the player has won or surpassed the goal number
 function crystalClicker(id, crystalNum) {
 		$(id).click(function(event) {
 		if (gameStarted) {
